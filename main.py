0ca676bae5f5ca65ab88105f116e7ab605b3e1f7
mrgizmo212 <info@truetradinggroup.com>
2024-08-04 17:23:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ async def research_direct(
 
 def run_fastapi():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", default="127.0.0.1", help="Host to run the server on")
+    parser.add_argument("--host", default="0.0.0.0", help="Host to run the server on")
     parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
     args = parser.parse_args()
     uvicorn.run(app, host=args.host, port=args.port)
